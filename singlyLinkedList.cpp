482d7c3e3e7a35bc15b794288a59642f75508edc
Fannandya <fannandya.sutan.ft24@mail.umy.ac.id>
2025-05-08 19:44:25 +0700

    membuat class

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
+
+class node
+{
+public:
+    int noMhs;
+    node *next;
+};
+
+class linkedList
+{
+    node *START;
+
+public:
+    linkedList()
+}
